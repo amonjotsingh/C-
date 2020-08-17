44d5413d8bd5fd11c07035acffc739bf64e89db4
Amonjot Singh Chhina <60524802+amonjotsingh@users.noreply.github.com>
2020-08-17 02:46:36 -0400

    Update source.c

--- queue/source.c
+++ queue/source.c
@@ -1,9 +1,9 @@
 /*
-* source.c
-*
-* Created on: Jun 15, 2020
-* Author: amonjot
-*/
+ * source.c
+ *
+ * Created on: Jun 15, 2020
+ * Author: amonjot
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -11,37 +11,37 @@
 //FUNCTION DECLARATION TO CHECK IF QUEUE IS EMPTY
 bool queue_empty(queue_t *s)
 {
-  if ((s->head) == (s->tail))
-  { 
-  return true;
-  }
-  else
-  {
-return false;
-  }
+	if ((s->head) == (s->tail))
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 /*
-* push() implementation
-*/
+ * push() implementation
+ */
 void push(queue_t *s, int x)
 {
-  s->data[(s->tail)++] = x;
-/*PUSH DATA AND THEN INCREMENT TO NEXT DATA LOCATION*/
-  return;
+	s->data[(s->tail)++] = x;
+	/*PUSH DATA AND THEN INCREMENT TO NEXT DATA LOCATION*/
+	return;
 }
 /*
-* dequeue() implementation
-*/
+ * dequeue() implementation
+ */
 int dequeue(queue_t *s)
 {
-  if (queue_empty(s))
-  {
-  printf("underflow error!");
-  exit(EXIT_FAILURE);
-  }
-  else
-  {
-  return (*s).data[(s->head)++];
-/* derefrencing the pointed address and then moving to next adress*/
-  }
-  }
+	if (queue_empty(s))
+	{
+		printf("underflow error!");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		return (*s).data[(s->head)++];
+		/* derefrencing the pointed address and then moving to next adress*/
+	}
+}
